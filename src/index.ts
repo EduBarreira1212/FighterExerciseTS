81707f1fd95795db9e1f0c2379bd1038b8339d95
Eduardo Barreira <edubarreira1212@gmail.com>
2023-08-19 02:56:33 -0300

    Uploaded to GitHub

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+class Lutador{
+    Nome: string;
+    Vida: number;
+    Ataque: number;
+
+    constructor (nome: string, vida: number, ataque: number){
+        this.Nome = nome;
+        this.Vida = vida;
+        this.Ataque = ataque;
+    }
+    
+}
+
+let lutador1 = new Lutador("Fulano", 10, 2);
+let lutador2 = new Lutador("Ciclano", 7, 3);
+let nomeComecar = lutador1.Nome;
+Lutar(lutador1, lutador2, nomeComecar);
+
+function Lutar(lutador1: Lutador, lutador2: Lutador, nome: string) {
+    if(nome == lutador1.Nome){
+        while(!(lutador1.Vida <= 0 || lutador2.Vida <= 0)){
+            lutador2.Vida -= lutador1.Ataque;
+            console.log(`${lutador1.Nome} atacou ${lutador2.Nome}`);
+            console.log(`${lutador2.Nome} agora tem ${lutador2.Vida} de vida`);
+            if(lutador2.Vida <= 0){
+                break;
+            }
+            lutador1.Vida -= lutador2.Ataque;
+            console.log(`${lutador2.Nome} atacou ${lutador1.Nome}`);
+            console.log(`${lutador1.Nome} agora tem ${lutador1.Vida} de vida`);
+            if(lutador1.Vida <= 0){
+                break;
+            }
+        }
+    }
+
+    else if(nome == lutador2.Nome){
+        while(!(lutador1.Vida <= 0 || lutador2.Vida <= 0)){
+            lutador1.Vida -= lutador2.Ataque;
+            console.log(`${lutador2.Nome} atacou ${lutador1.Nome}`);
+            console.log(`${lutador1.Nome} agora tem ${lutador1.Vida} de vida`);
+            if(lutador1.Vida <= 0){
+                break;
+            }
+            lutador2.Vida -= lutador1.Ataque;
+            console.log(`${lutador1.Nome} atacou ${lutador2.Nome}`);
+            console.log(`${lutador2.Nome} agora tem ${lutador2.Vida} de vida`);
+            if(lutador2.Vida <= 0){
+                break;
+            }
+        }
+    }
+
+    if(lutador1.Vida > lutador2.Vida){
+       console.log(`${lutador1.Nome} ganhou`);
+    }
+    else if(lutador1.Vida < lutador2.Vida){
+        console.log(`${lutador2.Nome} ganhou`);
+    }
+}
